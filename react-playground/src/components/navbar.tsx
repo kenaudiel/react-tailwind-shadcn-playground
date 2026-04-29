37c11f817828a25b84774a667b3f32338e6314b4
Kenneth <audielkenneth23@gmail.com>
2026-04-29 16:33:53 +0700

    fix dark mode

--- react-playground/src/components/navbar.tsx
+++ react-playground/src/components/navbar.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react"
 import { Bell, Brush, DollarSign, LogOut, Moon, Settings, User } from 'lucide-react'
 import { Avatar, AvatarFallback } from './ui/avatar'
+import { useTheme } from "./ui/theme-provider"
 import {
   NavigationMenu,
   NavigationMenuList,
@@ -15,6 +15,8 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Button } from "./ui/button"
+
 
 type NavbarProps = {
     href:string,
@@ -46,7 +48,7 @@ const ListItemMap:any = components.map((component) => {
 })
 
 export default function Navbar(){
-    const { isDark, toggle } = useDarkMode()
+    const { setTheme } = useTheme()
 
     return(
             <NavigationMenu className="gap-70 justify-between">
@@ -55,7 +57,24 @@ export default function Navbar(){
                 </NavigationMenuList>
 
                 <NavigationMenuList className="gap-2 w-60">
-                    <ListItem href="" onClick={toggle}><Moon /></ListItem>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <Moon />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem onClick={() => setTheme("light")}>
+                                Light
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("dark")}>
+                                Dark
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => setTheme("system")}>
+                                System
+                            </DropdownMenuItem>
+                    </DropdownMenuContent>
+                    </DropdownMenu>
                     <ListItem href=""><Bell /></ListItem>
                     <ListItem href=""><Settings /></ListItem>
                     <ListItem href="https://github.com/kenaudiel"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg></ListItem>
@@ -91,16 +110,4 @@ function ListItem({href, children, onClick}:NavbarProps){
             <NavigationMenuLink href={href} onClick={onClick}>{children}</NavigationMenuLink>
         </NavigationMenuItem>
     )
-}
-
-function useDarkMode() {
-  const [isDark, setIsDark] = useState(false)
-
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", isDark)
-  }, [isDark])
-
-  const toggle = () => setIsDark(prev => !prev)
-
-  return { isDark, toggle }
 }

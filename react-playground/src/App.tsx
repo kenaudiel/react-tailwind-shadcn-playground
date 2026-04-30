db1ab72ee12f86523e3f89a094e9a5da2f323124
Kenneth <audielkenneth23@gmail.com>
2026-04-30 21:57:19 +0700

    rendering list image cards

--- react-playground/src/App.tsx
+++ react-playground/src/App.tsx
@@ -2,6 +2,29 @@ import { CardImage } from "./components/cardimage";
 import Navbar from "./components/navbar";
 import { ThemeProvider } from "./components/ui/theme-provider";
 
+const cardImageItems:{title:string, description:string, buttonText:string}[] = [
+  {
+    title:"Card #1",
+    description:"This is a demonstration of card description.",
+    buttonText:"Learn More"
+  },
+  {
+    title:"Card #2",
+    description:"This is a demonstration of card description.",
+    buttonText:"View More"
+  },
+  {
+    title:"Card #3",
+    description:"This is a demonstration of card description.",
+    buttonText:"Learn More"
+  },
+  {
+    title:"Card #4",
+    description:"This is a demonstration of card description.",
+    buttonText:"View More"
+  },
+]
+
 export default function App(){
 	return(
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
@@ -9,13 +32,11 @@ export default function App(){
       
         <section className="bg-gray-300 p-20">
           <div className="grid grid-cols-4 grid-rows-1 grid-flow-row ">
-            <CardImage title="Card Title #1" description="This is a demonstration of card description." buttonText="Learn More"/>
-            <CardImage title="Card Title #2" description="This is the card description as demonstration." buttonText="View More"/>
-            <CardImage title="Card Title #3" description="This is the card description as demonstration." buttonText="Learn More"/>
-            <CardImage title="Card Title #4" description="This is the card description as demonstration." buttonText="View More"/>
+            {cardImageItems.map((card) => {
+              return <CardImage title={card.title} description={card.description} buttonText={card.buttonText}/>
+            })}
           </div>
         </section>
       </ThemeProvider>
 	)
 } 
-  
